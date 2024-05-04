392c3db58d7ce88e266758942fca7b8904d8d295
Latif <abdullatif.atci@trendyol.com>
2024-05-04 21:53:29 +0300

    add resources

--- Package.swift
+++ Package.swift
@@ -16,7 +16,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LatiFlex"),
+            name: "LatiFlex",
+        resources: [
+            .process("Resources/Assests.xcassets")
+        ]),
         .testTarget(
             name: "LatiFlexTests",
             dependencies: ["LatiFlex"]),
