a5f8713429fa3f28ea943c831df9b634d306aa6f
BanuKarakaya <nurbanukarakaya03@gmail.com>
2024-08-23 22:36:21 +0300

    updates to the copy url button and the latest look

--- Sources/LatiFlex/Views/Network/NetworkView/View/LatiFlexNetworkCell.swift
+++ Sources/LatiFlex/Views/Network/NetworkView/View/LatiFlexNetworkCell.swift
@@ -327,13 +327,18 @@ extension LatiFlexNetworkCell: LatiFlexNetworkCellInterface {
     func prepareCopyCurlButtonForDefaultState() {
         copyCurlButton.setTitle(Constant.copyCurlText, for: .normal)
         copyCurlButton.setTitleColor(.orange, for: .normal)
-        copyCurlButton.backgroundColor = UIColor.orange.withAlphaComponent(0.3)
+        copyCurlButtonView.backgroundColor = .orange.withAlphaComponent(0.1)
+        copyCurlButtonView.layer.borderColor = UIColor.orange.withAlphaComponent(0.3).cgColor
+       // copyCurlButton.backgroundColor = UIColor.orange.withAlphaComponent(0.3)
     }
     
     func prepareCopyCurlButtonForCopiedState() {
         copyCurlButton.setTitle(Constant.copiedText, for: .normal)
         copyCurlButton.setTitleColor(.systemGreen, for: .normal)
-        copyCurlButton.backgroundColor = UIColor.systemGreen.withAlphaComponent(0.3)
+      //  copyCurlButton.backgroundColor = UIColor.systemGreen.withAlphaComponent(0.3)
+        copyCurlButtonView.backgroundColor = Constant.customGreen.withAlphaComponent(0.1)
+        copyCurlButtonView.layer.borderColor = Constant.customGreen.cgColor
+        
     }
     
     func setTextToClipboard(_ text: String?) {
